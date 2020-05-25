a4cfd0ad29e7cea6429e2e124de2deb3efa1d91a
s1768094 <s1768094@ed.ac.uk>
2020-05-25 13:52:04 +0100

    clean up

--- cmavm.sh
+++ cmavm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-qemu-system-x86_64 -enable-kvm -kernel arch/x86_64/boot/bzImage -initrd ../pagewalk/initramfs.img -m 1G -append " cma=300M" 
+qemu-system-x86_64 -enable-kvm -kernel arch/x86_64/boot/bzImage -initrd initramfs.img -m 1G -append "cma=400M" 
